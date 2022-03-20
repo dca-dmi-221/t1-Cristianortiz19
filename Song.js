0f27525c4af3d45247bcd23ebeb10cf78f218c2e
Cristianortiz19 <cristclar012@gmail.com>
2022-03-20 17:58:02 -0500

    Parametos de la clase song creaditos 😍😎😍😍

--- Song.js
+++ Song.js
@@ -1,3 +1,17 @@
 class Song{
-    constructor(){}
+    constructor({
+        name,
+        artist,
+        genre,
+        date,
+        duration,
+        soundFile
+    }){
+        this._name = name;
+        this._artist = artist;
+        this._genre = genre;
+        this._date = date;
+        this._duration = duration;
+        this._soundFile = soundFile;
+    }
 }
