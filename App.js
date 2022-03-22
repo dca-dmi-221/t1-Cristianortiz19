2164693abcab04c43896a96d87f6dc712c20fb6b
Cristianortiz19 <cristclar012@gmail.com>
2022-03-21 20:32:39 -0500

    Cargando más cancioncitas

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ class App{
     }
     loadSongs(){
         this._songs[0] = loadSound('/Songs/Slowdive - When the sun hits.mp3');
+        this._songs[1] = loadSound('/Songs/Surf Curse - Freaks.mp3')
     }
     get songs(){
         return this._songs;
