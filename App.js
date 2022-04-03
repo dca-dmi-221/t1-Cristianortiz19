3c8f778b1861212471bf47046907f7832d639e1c
Cristianortiz19 <cristclar012@gmail.com>
2022-04-03 12:26:36 -0500

    Todas las canciones cargadas y creadas

--- App.js
+++ App.js
@@ -17,6 +17,14 @@ class App{
         this._songsFiles[4] = loadSound('/Songs/Pantysito.mp3');
         this._songsFiles[5] = loadSound('/Songs/I Think I Like It.mp3');
         this._songsFiles[6] = loadSound('/Songs/Lady.mp3');
+        this._songsFiles[7] = loadSound('/Songs/Chandelier.mp3');
+        this._songsFiles[8] = loadSound('/Songs/Demasiadas Mujeres.mp3');
+        this._songsFiles[9] = loadSound('/Songs/Jugaste y Sufri.mp3');
+        this._songsFiles[10] = loadSound('/Songs/METALLICA.mp3');
+        this._songsFiles[11] = loadSound('/Songs/Softcore.mp3');
+        this._songsFiles[12] = loadSound('/Sudno.mp3');
+        this._songsFiles[13] = loadSound('/Songs/The Less I Know The Better.mp3');
+        this._songsFiles[14] = loadSound('/Songs/High Enough.mp3');
     }
     songDates(){
         this._songs.push(new Song({
@@ -56,16 +64,88 @@ class App{
             artist: 'Feid, Alejo & Robi',
             genre: 'Reggaeton',
             date: '2022',
-            duration: '110',
+            duration: '237',
             soundFile: this._songsFiles[4]
         }));
         this._songs.push(new Song({
-            name: 'Fahrradsattel',
-            artist: 'Pissel',
-            genre: 'Rock',
-            date: '2021',
-            duration: '110',
+            name: 'I think I Like It',
+            artist: 'Fake Blood',
+            genre: 'Electronic',
+            date: '2015',
+            duration: '335',
             soundFile: this._songsFiles[5]
+        }));
+        this._songs.push(new Song({
+            name: 'Lady (Hear Me Tonight)',
+            artist: 'Modjo',
+            genre: 'Disco',
+            date: '2000',
+            duration: '221',
+            soundFile: this._songsFiles[6]
+        }));
+        this._songs.push(new Song({
+            name: 'Chandelier',
+            artist: 'Will Paquin',
+            genre: 'Alternative/independent',
+            date: '2021',
+            duration: '129',
+            soundFile: this._songsFiles[7]
+        }));
+        this._songs.push(new Song({
+            name: 'Demasiadas mujeres',
+            artist: 'C. Tangana',
+            genre: 'Latin Urban, R&B alternative',
+            date: '2021',
+            duration: '167',
+            soundFile: this._songsFiles[8]
+        }));
+        this._songs.push(new Song({
+            name: 'Jugaste Y Sufrí',
+            artist: 'Eslabón Armado',
+            genre: 'Corridos Tumbados',
+            date: '2020',
+            duration: '275',
+            soundFile: this._songsFiles[9]
+        }));
+        this._songs.push(new Song({
+            name: 'METALLICA',
+            artist: 'Yung Beef',
+            genre: 'Trap',
+            date: '2021',
+            duration: '225',
+            soundFile: this._songsFiles[10]
+        }));
+        this._songs.push(new Song({
+            name: 'Softcore',
+            artist: 'The Neighbourhood',
+            genre: 'Alternative',
+            date: '2018',
+            duration: '210',
+            soundFile: this._songsFiles[11]
+        }));
+        this._songs.push(new Song({
+            name: 'Sudno',
+            artist: 'Molchat Doma',
+            genre: 'Post Punk',
+            date: '2018',
+            duration: '145',
+            soundFile: this._songsFiles[12]
+        }));
+        this._songs.push(new Song({
+            name: 'The Less I know The Better',
+            artist: 'Tame Impala',
+            genre: 'Alternative',
+            date: '2015',
+            duration: '227',
+            soundFile: this._songsFiles[13]
+        }));
+        this._songs.push(new Song({
+            name: 'High Enough',
+            artist: 'K.Flay',
+            genre: 'Indie Rock',
+            date: '2017',
+            duration: '290',
+            soundFile: this._songsFiles[14]
         }));
     }
     interface(){
